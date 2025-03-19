3ba69b2d5b1aa1f6db3bfd9833bd67fbabb5021c
Juan Manuel Benitez <jm_benitez@outlook.com>
2025-03-19 14:28:01 -0500

    Improve Divide method with error handling for zero
    Updated the Divide method in the Calculator namespace to include error handling for division by zero. A message box now alerts the user when attempting to divide by zero, and the method returns 0 in such cases, ensuring safer calculations.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -136,7 +136,16 @@ namespace Calculator
 
         public static double Multiply(double n1, double n2) => n1 * n2;
 
-        public static double Divide(double n1, double n2) => n1 / n2;
+        public static double Divide(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+                MessageBox.Show("Division by 0 is not supported", "Wrong Operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+               
+         return   n1 / n2;
+        }
 
     }
 
